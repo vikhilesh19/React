a6c24f634417a87722b4751b7cde312ae605e4e2
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-06-22 15:10:58 +0530

    children

--- learning_fragments/src/App.jsx
+++ learning_fragments/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import FoodItems from './components/FoodItems';
+import Container from './components/Container';
 
 function App() {
   let fooditems=['Dal','Vegetables','Roti','Rice','Milk'];
@@ -10,17 +11,20 @@ function App() {
   }
   return (
     <React.Fragment>
-      <h1>Healthy Food</h1>
-      {fooditems.length==5? <h4>5 items</h4>:null}
-      <ul className="list-group">
-        <li className="list-group-item">Dal</li>
-        <li className="list-group-item">Vegetables</li>
-        <li className="list-group-item">Roti</li>
-        <li className="list-group-item">Rice</li>
-        <li className="list-group-item">Milk</li>
-      </ul>
       <h2>Using map</h2>
       <FoodItems></FoodItems>
+      
+      <Container>
+        <h1>Healthy Food</h1>
+        {fooditems.length==5? <h4>5 items</h4>:null}
+        <ul className="list-group">
+          <li className="list-group-item">Dal</li>
+          <li className="list-group-item">Vegetables</li>
+          <li className="list-group-item">Roti</li>
+          <li className="list-group-item">Rice</li>
+          <li className="list-group-item">Milk</li>
+        </ul>
+      </Container>
       
     </React.Fragment>
   )
---
+++ learning_fragments/src/components/Container.jsx
@@ -0,0 +1,5 @@
+import styles from "./container.module.css"
+const Container=(props)=>{
+    return <div className={styles.container}>{props.children}</div>
+}
+export default Container;
